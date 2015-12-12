e0ac8e0bd93a714fbb05889f20b82e8c810c4df5
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-12-12 21:04:07 +0100

    Added some control of the user input.

--- week3/caesar/caesar.c
+++ week3/caesar/caesar.c
@@ -1,8 +1,34 @@
 /*
  * caesar.c
- *
- *  Created on: 12 dec. 2015
- *      Author: Henrik Samuelsson
  */
+#include <stdlib.h>
 
+int
+main (int argc, char* argv[])
+{
+  int returnValue = 0;
+  int k;  /* number to be used when encoding */
 
+  if (returnValue == 0)
+  {
+    /* the user is only allowed to enter exactly one argument */
+    if (argv != 2)
+    {
+      printf("Exactly one argument shall be provided.");
+      returnValue = 1;
+    }
+  }
+
+  if (returnValue == 0)
+  {
+    k = atoi(argv[1]);
+    /* k shall be a positive integer */
+    if (k < 1)
+    {
+      printf("The provided argument must be a positive integer.");
+      returnValue = 1;
+    }
+  }
+
+  return returnValue;
+}
